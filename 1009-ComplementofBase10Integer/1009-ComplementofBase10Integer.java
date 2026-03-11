ea2a97bae69bb13702650834f38509bc1ecfec14
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-03-11 12:15:46 +0530

    File updated at 3/11/2026, 12:15:47 PM

--- 1009-ComplementofBase10Integer/1009-ComplementofBase10Integer.java
+++ 1009-ComplementofBase10Integer/1009-ComplementofBase10Integer.java
@@ -1,15 +1,15 @@
-// Last updated: 3/11/2026, 12:08:54 PM
+// Last updated: 3/11/2026, 12:15:47 PM
 1class Solution {
 2    public int bitwiseComplement(int n) {
 3        String str=Integer.toBinaryString(n);
-4        String res="";
+4        StringBuilder res=new StringBuilder();
 5        for(int i=0;i<str.length();i++){
 6            if(str.charAt(i)=='0'){
-7                res+="1";
+7                res.append("1");
 8            }else{
-9                res+="0";
+9                res.append("0");
 10            }
 11        } 
-12        return(Integer.parseInt(res,2));
+12        return(Integer.parseInt(res.toString(),2));
 13    }
 14}
