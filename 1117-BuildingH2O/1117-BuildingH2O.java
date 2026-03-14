d570984368e501778f316d4c5beb4fd363bae2e1
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-03-14 14:41:42 +0530

    File updated at 3/14/2026, 2:41:43 PM

--- 1117-BuildingH2O/1117-BuildingH2O.java
+++ 1117-BuildingH2O/1117-BuildingH2O.java
@@ -1,32 +1,24 @@
-// Last updated: 3/14/2026, 2:36:10 PM
+// Last updated: 3/14/2026, 2:41:43 PM
 1class H2O{
 2    int hcount=0;
 3    public synchronized void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
 4        while(hcount==2){
-5            try{
-6                wait();
-7            }catch(InterruptedException e){
-8                System.out.print(e.getMessage());
-9            }
-10        }
-11        hcount++;
-12        releaseHydrogen.run();
-13        notifyAll();
-14
-15    }
-16    public synchronized void oxygen(Runnable releaseOxygen) throws InterruptedException {
-17        while(hcount<2){
-18            try{
-19                wait();
-20            }catch(InterruptedException e){
-21                System.out.print(e.getMessage());
-22            }
-23        }
-24        hcount=0;
-25        releaseOxygen.run();
-26        notifyAll();
-27
-28    }
-29}
-30
-31
+5                wait();   
+6        }
+7        hcount++;
+8        releaseHydrogen.run();
+9        notifyAll();
+10
+11    }
+12    public synchronized void oxygen(Runnable releaseOxygen) throws InterruptedException {
+13        while(hcount<2){
+14                wait();
+15        }
+16        hcount=0;
+17        releaseOxygen.run();
+18        notifyAll();
+19
+20    }
+21}
+22
+23
