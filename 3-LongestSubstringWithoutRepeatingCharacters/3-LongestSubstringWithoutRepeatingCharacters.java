229790bf42e8a35da46948f17d3a8c4abec4d19a
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-03-16 14:40:31 +0530

    File updated at 3/16/2026, 2:40:32 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -1,24 +1,24 @@
-// Last updated: 3/16/2026, 2:01:55 PM
+// Last updated: 3/16/2026, 2:40:32 PM
 1class Solution {
-2     static{
-3    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-4            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-5                fw.write("0");
-6            } catch (Exception e) {
-7            }
-8        }));
-9     }
-10    public List<List<String>> groupAnagrams(String[] strs) {
-11        Map<String,List<String>> map=new LinkedHashMap<>();
-12        for(String s:strs){
-13            char[] k=s.toCharArray();
-14            Arrays.sort(k);
-15            String key=new String(k);
-16            if(!map.containsKey(key)){
-17                map.put(key,new ArrayList<>());
-18            }
-19            map.get(key).add(s);
+2    public int romanToInt(String s) {
+3        int arr[]=new int[26];
+4        arr['I'-'A']=1;
+5        arr['V'-'A']=5;
+6        arr['X'-'A']=10;
+7        arr['L'-'A']=50;
+8        arr['C'-'A']=100;
+9        arr['D'-'A']=500;
+10        arr['M'-'A']=1000;
+11        char ch[]=s.toCharArray();
+12        int res=0;
+13        for(int i=0;i<s.length();i++){
+14            if(i!=s.length()-1&&arr[ch[i]-'A']<arr[ch[i+1]-'A']){
+15                res+=arr[ch[i+1]-'A']-arr[ch[i]-'A'];
+16                i++;
+17            }else{
+18                res+=arr[ch[i]-'A'];
+19            }
 20        }
-21        return new ArrayList<>(map.values());
+21        return res;
 22    }
 23}
