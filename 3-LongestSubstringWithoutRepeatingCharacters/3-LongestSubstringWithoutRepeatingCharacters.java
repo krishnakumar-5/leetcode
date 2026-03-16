113315445d3a9107b9b4d9c8ef6ed0972895aba6
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-03-16 14:00:36 +0530

    File updated at 3/16/2026, 2:00:37 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -1,25 +1,16 @@
-// Last updated: 3/16/2026, 1:48:20 PM
+// Last updated: 3/16/2026, 2:00:37 PM
 1class Solution {
-2    static{
-3    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-4            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-5                fw.write("0");
-6            } catch (Exception e) {
-7            }
-8        }));
-9     }
-10    public int lengthOfLongestSubstring(String s) {
-11        int[] freq=new int[256];
-12        int max=0;
-13        int left=0;
-14        for(int r=0;r<s.length();r++){
-15            freq[s.charAt(r)]++;
-16            while(freq[s.charAt(r)]>1){
-17                freq[s.charAt(left)]--;
-18                left++;
-19            }
-20            max=Math.max(max,r-left+1);
-21        }
-22        return max;
-23    }
-24}
+2    public List<List<String>> groupAnagrams(String[] strs) {
+3        Map<String,List<String>> map=new LinkedHashMap<>();
+4        for(String s:strs){
+5            char[] k=s.toCharArray();
+6            Arrays.sort(k);
+7            String key=new String(k);
+8            if(!map.containsKey(key)){
+9                map.put(key,new ArrayList<>());
+10            }
+11            map.get(key).add(s);
+12        }
+13        return new ArrayList<>(map.values());
+14    }
+15}
