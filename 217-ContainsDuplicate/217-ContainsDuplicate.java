4d39e939239aa4bd676f85989326229f8912ad5b
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-03-16 10:27:41 +0530

    File updated at 3/16/2026, 10:27:41 AM

--- 217-ContainsDuplicate/217-ContainsDuplicate.java
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -1,26 +1,13 @@
-// Last updated: 2/13/2026, 10:18:13 AM
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new LinkedHashSet();  
-        for (int num : nums) {
-            if (set.contains(num)) {
-                return true; 
-            }
-            set.add(num);
-        }
-        return false; 
-        // int count=0;
-        // for(int i=0;i<nums.length;i++){
-        //     for(int j=i+1;j<=nums.length-1;j++){
-        //      if(nums[i]==nums[j]){
-        //         count++;
-        //      }
-        //     }
-        // }
-        // if(count==0){
-        //     return false;
-        // }else{
-        //     return true;
-        // }
-    }
-}
+// Last updated: 3/16/2026, 10:27:41 AM
+1class Solution {
+2    public boolean containsDuplicate(int[] nums) {
+3        Set<Integer> set=new LinkedHashSet<>();
+4        for(int i=0;i<nums.length;i++){
+5            set.add(nums[i]);
+6        }
+7        if(set.size()!=nums.length){
+8            return true;
+9        }
+10        return false;
+11    }
+12}
