af87c2e95cba2805ba2bc3a8038c1dfa84edd21e
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-03-17 12:18:57 +0530

    File updated at 3/17/2026, 12:18:57 PM

--- 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.java
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.java
@@ -1,4 +1,4 @@
-// Last updated: 3/17/2026, 12:18:46 PM
+// Last updated: 3/17/2026, 12:18:57 PM
 class Solution {
     public int evalRPN(String[] tokens) {
        Stack<Integer> stack = new Stack<>();
