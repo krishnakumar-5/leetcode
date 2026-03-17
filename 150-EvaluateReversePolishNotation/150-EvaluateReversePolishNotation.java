065e9895765e66ef61deab4e571e55cb75f12515
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-03-17 12:18:08 +0530

    File updated at 3/17/2026, 12:18:08 PM

--- 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.java
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.java
@@ -1,38 +1,35 @@
-// Last updated: 3/17/2026, 12:08:12 PM
-1class Solution {
-2    static {
-3        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-4            try (FileWriter fw = new FileWriter("display_runtime.txt")) {
-5                fw.write("0");
-6            } catch (Exception e) {
-7            }
-8        }));
-9    }
-10    public int evalRPN(String[] tokens) {
-11        Stack<String> st=new Stack<>();
-12        for(int i=0;i<tokens.length;i++){
-13            if(!tokens[i].equals("*")&&!tokens[i].equals("/")&&!tokens[i].equals("+")&&!tokens[i].equals("-")){
-14                 st.push(tokens[i]);
-15            }else{
-16                int res=0;
-17                int a=Integer.parseInt(st.pop());
-18                int b=Integer.parseInt(st.pop());
-19                if(tokens[i].equals("+")){
-20                    res=a+b;
-21                    st.push(String.valueOf(res));
-22                }else if(tokens[i].equals("-")){
-23                   res=b-a;
-24                    st.push(String.valueOf(res)); 
-25                }else if(tokens[i].equals("*")){
-26                    res=a*b;
-27                    st.push(String.valueOf(res));
-28                }else if(tokens[i].equals("/")){
-29                    res=b/a;
-30                    st.push(String.valueOf(res));
-31                }
-32            }
-33        }
-34        return Integer.parseInt(st.pop());
-35        
-36    }
-37}
+// Last updated: 3/17/2026, 12:18:08 PM
+class Solution {
+    public int evalRPN(String[] tokens) {
+       Stack<Integer> stack = new Stack<>();
+
+        for (String c : tokens) {
+            if (c.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (c.equals("-")) {
+                int second = stack.pop();
+                int first = stack.pop();
+                stack.push(first - second);
+            } else if (c.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (c.equals("/")) {
+                int second = stack.pop();
+                int first = stack.pop();
+                stack.push(first / second);
+            } else {
+                stack.push(Integer.parseInt(c));
+            }
+        }
+
+        return stack.peek();            
+
+        }
+ static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
+                fw.write("0");
+            } catch (Exception e) {
+            }
+        }));
+}
+}
