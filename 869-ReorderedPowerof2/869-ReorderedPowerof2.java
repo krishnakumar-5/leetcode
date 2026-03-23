cd6847b5e8bb8aba489d6d2b715571d446e0b49a
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-03-23 12:02:16 +0530

    File updated at 3/23/2026, 12:02:17 PM

--- 869-ReorderedPowerof2/869-ReorderedPowerof2.java
+++ 869-ReorderedPowerof2/869-ReorderedPowerof2.java
@@ -1,26 +1,45 @@
-// Last updated: 3/23/2026, 11:31:42 AM
+// Last updated: 3/23/2026, 12:02:17 PM
 1/**
 2 * Definition for singly-linked list.
-3 * class ListNode {
+3 * public class ListNode {
 4 *     int val;
 5 *     ListNode next;
-6 *     ListNode(int x) {
-7 *         val = x;
-8 *         next = null;
-9 *     }
-10 * }
-11 */
-12public class Solution {
-13    public boolean hasCycle(ListNode head) {
-14        ListNode slow=head;
-15        ListNode fast=head;
-16        while(fast!=null&&fast.next!=null){
-17            slow=slow.next;
-18            fast=fast.next.next;
-19            if(slow==fast){
-20                return true;
-21            }
-22        }
-23        return false;
-24    }
-25}
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+13        if(list1==null&&list2==null){
+14            return null;
+15        }else{
+16            if(list1==null){
+17                return list2;
+18            }else if(list2==null){
+19                return list1;
+20            }else{
+21                ListNode dum=new ListNode(0);
+22                ListNode temp=dum;
+23                ListNode c1=list1;
+24                ListNode c2=list2;
+25                while(c1!=null&&c2!=null){
+26                    if(c1.val>c2.val){
+27                        temp.next=c2;
+28                        c2=c2.next;
+29                    }else{
+30                        temp.next=c1;
+31                        c1=c1.next;
+32                    }
+33                    temp=temp.next;
+34                }
+35                if(c1!=null){
+36                    temp.next=c1;
+37                }else{
+38                    temp.next=c2;
+39                }
+40                return dum.next;
+41            }
+42        }
+43    }
+44}
