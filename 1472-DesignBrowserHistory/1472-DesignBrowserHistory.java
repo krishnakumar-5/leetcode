33e64d9b42eacaa5541c7689631f78f61d309932
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-03-24 14:44:55 +0530

    File updated at 3/24/2026, 2:44:55 PM

--- 1472-DesignBrowserHistory/1472-DesignBrowserHistory.java
+++ 1472-DesignBrowserHistory/1472-DesignBrowserHistory.java
@@ -1,48 +1,31 @@
-// Last updated: 3/24/2026, 2:10:28 PM
-1class BrowserHistory {
-2    class Node{
-3        String url;
-4        Node prev;
-5        Node next;
-6        Node(String url){
-7            this.url=url;
-8            prev=null;
-9            next=null;
-10        }
-11    }
-12    Node cur;
-13    public BrowserHistory(String homepage) {
-14        cur=new Node(homepage);
-15    }
-16    
-17    public void visit(String url) {
-18        Node nn=new Node(url);
-19        cur.next=nn;
-20        nn.prev=cur;
-21        cur=nn;
-22    }
-23    
-24    public String back(int steps) {
-25        while(cur.prev!=null&&steps>0){
-26            cur=cur.prev;
-27            steps--;
-28        }
-29        return cur.url;
-30    }
-31    
-32    public String forward(int steps) {
-33        while(cur.next!=null&&steps>0){
-34            cur=cur.next;
-35            steps--;
-36        }
-37        return cur.url;
-38    }
-39}
-40
-41/**
-42 * Your BrowserHistory object will be instantiated and called as such:
-43 * BrowserHistory obj = new BrowserHistory(homepage);
-44 * obj.visit(url);
-45 * String param_2 = obj.back(steps);
-46 * String param_3 = obj.forward(steps);
-47 */
+// Last updated: 3/24/2026, 2:44:55 PM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode oddEvenList(ListNode head) {
+13            if(head==null||head.next==null) 
+14                return head;
+15            ListNode odd=head;
+16            ListNode even=head.next;
+17            ListNode evenhead=even;
+18            while(even!=null&&even.next!=null){
+19                odd.next=odd.next.next;
+20                odd=odd.next;
+21                even.next=even.next.next;
+22                even=even.next;
+23                
+24            }
+25            odd.next=evenhead;
+26            return head;
+27        
+28        
+29    }
+30}
