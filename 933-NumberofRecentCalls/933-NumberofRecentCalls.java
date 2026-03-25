5f10b50bfcb8060e01458e16e0091c593163eb8b
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-03-25 18:59:40 +0530

    File updated at 3/25/2026, 6:59:41 PM

--- 933-NumberofRecentCalls/933-NumberofRecentCalls.java
+++ 933-NumberofRecentCalls/933-NumberofRecentCalls.java
@@ -1,19 +1,18 @@
-// Last updated: 3/25/2026, 6:58:18 PM
+// Last updated: 3/25/2026, 6:59:41 PM
 1class RecentCounter {
-2    Stack<Integer> stack = new Stack<>();
+2    Queue<Integer> q = new LinkedList<>();
 3
 4    public RecentCounter() {
 5        
 6    }
-7
+7    
 8    public int ping(int t) {
-9        int c = t - 3000;
-10        stack.push(t);
-11
-12        while (!stack.isEmpty() && stack.get(0) < c) {
-13            stack.remove(0);   
-14        }
-15
-16        return stack.size();
-17    }
-18}
+9        q.add(t);
+10
+11        while (!q.isEmpty() && q.peek() < t - 3000) {
+12            q.poll();
+13        }
+14
+15        return q.size();
+16    }
+17}
