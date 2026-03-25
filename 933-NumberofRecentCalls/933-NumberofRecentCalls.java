3cc44325ea93d7d0e70d5a32c4246aca713f2b8d
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-03-25 18:58:17 +0530

    File updated at 3/25/2026, 6:58:18 PM

--- 933-NumberofRecentCalls/933-NumberofRecentCalls.java
+++ 933-NumberofRecentCalls/933-NumberofRecentCalls.java
@@ -1,4 +1,4 @@
-// Last updated: 3/25/2026, 6:55:30 PM
+// Last updated: 3/25/2026, 6:58:18 PM
 1class RecentCounter {
 2    Stack<Integer> stack = new Stack<>();
 3
@@ -10,12 +10,10 @@
 9        int c = t - 3000;
 10        stack.push(t);
 11
-12        // ✅ Remove outdated elements from bottom
-13        while (!stack.isEmpty() && stack.firstElement() < c) {
-14            stack.remove(0);   // remove oldest
-15        }
-16
-17        // Now all elements are within range
-18        return stack.size();
-19    }
-20}
+12        while (!stack.isEmpty() && stack.get(0) < c) {
+13            stack.remove(0);   
+14        }
+15
+16        return stack.size();
+17    }
+18}
