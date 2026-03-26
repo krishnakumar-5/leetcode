60ef6b426b567f9426f11747873d29ff576f25f6
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-03-26 12:00:25 +0530

    File updated at 3/26/2026, 12:00:24 PM

--- 1700-NumberofStudentsUnabletoEatLunch/1700-NumberofStudentsUnabletoEatLunch.java
+++ 1700-NumberofStudentsUnabletoEatLunch/1700-NumberofStudentsUnabletoEatLunch.java
@@ -1,46 +1,20 @@
-// Last updated: 3/26/2026, 11:41:13 AM
+// Last updated: 3/26/2026, 12:00:24 PM
 1class Solution {
-2    public List<List<String>> res=new ArrayList<>();
-3    public List<List<String>> solveNQueens(int n) {
-4        char[][] board=new char[n][n];
-5        for(int i=0;i<n;i++){
-6            Arrays.fill(board[i],'.');
-7        }
-8        backtrack(board,0);
-9        return res;
-10    }
-11    public void backtrack(char[][] board,int row){
-12        if(row==board.length){
-13            ArrayList<String> l=new ArrayList<>();
-14            for(int i=0;i<board.length;i++){
-15                l.add(new String(board[i]));
-16            }
-17            res.add(l);
-18            return;
-19        }
-20        for(int col=0;col<board[row].length;col++){
-21            if(isSafe(board,row,col)){
-22                board[row][col]='Q';
-23                backtrack(board,row+1);
-24                board[row][col]='.';
-25            }
-26        }
-27        
-28    }
-29    public  boolean isSafe(char[][] board,int row,int col){
-30        for(int i=0;i<row;i++){
-31            if(board[i][col]=='Q'){
-32                return false;
-33            }
-34            for(int j=0;j<board[row].length;j++){
-35                if(board[i][j]=='Q'){
-36                    if(Math.abs(row-i)==Math.abs(col-j)){
-37                        return false;
-38                    }
-39                    break;
-40                }
-41            }
-42        }
-43        return true;
-44    }
-45}
+2    public List<String> res=new ArrayList<>();
+3    public List<String> generateParenthesis(int n) {
+4        gen(0,0,"",n);
+5        return res;
+6    }
+7    public void gen(int op,int cl,String s,int n){
+8        if(s.length()==n*2){
+9            res.add(s);
+10            return;
+11        }
+12        if(op<n){ //Generate "("
+13            gen(op+1,cl,s+"(",n);
+14        }
+15        if(cl<op){ //Generate ")"
+16            gen(op,cl+1,s+")",n);
+17        }
+18    }
+19}
