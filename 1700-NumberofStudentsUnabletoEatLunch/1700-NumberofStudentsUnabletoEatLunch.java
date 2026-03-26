17b18d655875896a5b3c221ce2956fac0480c771
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-03-26 11:41:14 +0530

    File updated at 3/26/2026, 11:41:13 AM

--- 1700-NumberofStudentsUnabletoEatLunch/1700-NumberofStudentsUnabletoEatLunch.java
+++ 1700-NumberofStudentsUnabletoEatLunch/1700-NumberofStudentsUnabletoEatLunch.java
@@ -1,28 +1,46 @@
-// Last updated: 3/26/2026, 9:24:30 AM
+// Last updated: 3/26/2026, 11:41:13 AM
 1class Solution {
-2    public int countStudents(int[] students, int[] sandwiches) {
-3        Stack<Integer> st=new Stack<>();
-4        Queue<Integer> q=new LinkedList<>();
-5        for(int i=0;i<students.length;i++){
-6            q.add(students[i]);
+2    public List<List<String>> res=new ArrayList<>();
+3    public List<List<String>> solveNQueens(int n) {
+4        char[][] board=new char[n][n];
+5        for(int i=0;i<n;i++){
+6            Arrays.fill(board[i],'.');
 7        }
-8        for(int i=sandwiches.length-1;i>=0;i--){
-9            st.push(sandwiches[i]);
-10        }
-11        int rej=0;
-12        while(!st.isEmpty()&&!q.isEmpty()){
-13            if(st.peek()==q.peek()){
-14                st.pop();
-15                q.remove();
-16                rej=0;
-17            }else{
-18                q.add(q.remove());
-19                rej++;
-20                if(rej==q.size()){
-21                    return q.size();
-22                }
-23            }
-24        }
-25        return q.size();
-26    }
-27}
+8        backtrack(board,0);
+9        return res;
+10    }
+11    public void backtrack(char[][] board,int row){
+12        if(row==board.length){
+13            ArrayList<String> l=new ArrayList<>();
+14            for(int i=0;i<board.length;i++){
+15                l.add(new String(board[i]));
+16            }
+17            res.add(l);
+18            return;
+19        }
+20        for(int col=0;col<board[row].length;col++){
+21            if(isSafe(board,row,col)){
+22                board[row][col]='Q';
+23                backtrack(board,row+1);
+24                board[row][col]='.';
+25            }
+26        }
+27        
+28    }
+29    public  boolean isSafe(char[][] board,int row,int col){
+30        for(int i=0;i<row;i++){
+31            if(board[i][col]=='Q'){
+32                return false;
+33            }
+34            for(int j=0;j<board[row].length;j++){
+35                if(board[i][j]=='Q'){
+36                    if(Math.abs(row-i)==Math.abs(col-j)){
+37                        return false;
+38                    }
+39                    break;
+40                }
+41            }
+42        }
+43        return true;
+44    }
+45}
