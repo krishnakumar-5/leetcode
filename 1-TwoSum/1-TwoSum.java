43099494a33bf6f52f96f016ba73383afe56f014
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-04-03 19:03:46 +0530

    File updated at 4/3/2026, 7:03:49 PM

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,22 +1,22 @@
-// Last updated: 2/13/2026, 10:18:31 AM
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        // for (int i = 0; i < nums.length - 1; i++) {
-        //     int c = i + 1;
-        //     int d = nums[i] + nums[c];
-        //     if (target == d) {
-        //         return new int[]{i, c}; 
-        //     }
-        // }
-        // return new int[]{}; 
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    return new int[]{i,j};
-                }
-
-            }
-        }
-        return new int[]{};
-    }
-}
+// Last updated: 4/3/2026, 7:03:49 PM
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        int l=0;
+4        int r=1;
+5        int res[]=new int[2];
+6        while(l!=nums.length){
+7            if(nums[l]+nums[r]==target&&l!=r){
+8                res[0]=l;
+9                res[1]=r;
+10                return res;
+11            }
+12            r++;
+13            if(r==nums.length){
+14                r=0;
+15                l++;
+16            }
+17        }
+18        return res;
+19        
+20    }
+21}
