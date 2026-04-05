4d365ba8e21b7fa51332b6b77d015580b158efdc
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-04-05 19:16:08 +0530

    File updated at 4/5/2026, 7:16:08 PM

--- 53-MaximumSubarray/53-MaximumSubarray.java
+++ 53-MaximumSubarray/53-MaximumSubarray.java
@@ -1,15 +1,12 @@
-// Last updated: 4/5/2026, 7:15:41 PM
+// Last updated: 4/5/2026, 7:16:08 PM
 1class Solution {
-2    public int maxProfit(int[] prices) {
-3        int res=prices[0];
-4        int pro=0;
-5        for(int i=1;i<prices.length;i++){
-6             if(prices[i]<res){
-7                res=prices[i];
-8             }else if(prices[i]-res>pro){
-9                pro=prices[i]-res;
-10             }
-11        }
-12        return pro;
-13    }
-14}
+2    public int maxSubArray(int[] nums) {
+3        int max=nums[0];
+4        int res=nums[0];
+5        for(int i=1;i<nums.length;i++){
+6            max=Math.max(max+nums[i],nums[i]);
+7            res=Math.max(res,max);
+8        }
+9        return res;
+10    }
+11}
