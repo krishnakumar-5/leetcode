f27d4e40f964f79b644d63d42d6efd72352d8e31
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-04-05 19:18:06 +0530

    File updated at 4/5/2026, 7:18:06 PM

--- 283-MoveZeroes/283-MoveZeroes.java
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -1,13 +1,13 @@
-// Last updated: 2/13/2026, 10:18:08 AM
-class Solution {
-    public void moveZeroes(int[] nums) {
-        int insertPos = 0;
-
-        for (int num : nums)
-            if (num != 0)
-                nums[insertPos++] = num;
-
-        while (insertPos < nums.length)
-            nums[insertPos++] = 0;
-    }
-}
+// Last updated: 4/5/2026, 7:18:06 PM
+1class Solution {
+2    public void moveZeroes(int[] nums) {
+3        int insertPos = 0;
+4
+5        for (int num : nums)
+6            if (num != 0)
+7                nums[insertPos++] = num;
+8
+9        while (insertPos < nums.length)
+10            nums[insertPos++] = 0;
+11    }
+12}
