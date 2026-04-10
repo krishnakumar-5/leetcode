448cf2879f75ab2a09c28e7ab32bb8079cb227b8
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-04-10 20:28:01 +0530

    File updated at 4/10/2026, 8:28:01 PM

--- 724-FindPivotIndex/724-FindPivotIndex.java
+++ 724-FindPivotIndex/724-FindPivotIndex.java
@@ -1,13 +1,20 @@
-// Last updated: 4/10/2026, 7:46:38 PM
+// Last updated: 4/10/2026, 8:28:01 PM
 1class Solution {
-2    public int pivotIndex(int[] nums) {
-3        int totalSum = 0;
-4        int leftsum = 0;
-5        for (int ele : nums)
-6            totalSum += ele;
-7        for (int i = 0; i < nums.length; leftsum += nums[i++])
-8            if (leftsum * 2 == totalSum - nums[i])
-9                return i;       
-10        return -1;      
-11    }   
-12}
+2    public int[] productExceptSelf(int[] nums) {
+3    int n = nums.length;
+4    int[] result = new int[n];
+5
+6    result[0] = 1;
+7    for (int i = 1; i < n; i++) {
+8        result[i] = result[i - 1] * nums[i - 1];
+9    }
+10
+11    int right = 1;
+12    for (int i = n - 1; i >= 0; i--) {
+13        result[i] = result[i] * right;
+14        right *= nums[i];
+15    }
+16
+17    return result;
+18    }
+19}
