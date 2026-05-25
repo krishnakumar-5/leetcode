2e6c76702ca2a1b2dfb66e9dfd8b2d0b2802868e
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-05-25 16:21:08 +0530

    File updated at 5/25/2026, 4:21:14 PM

--- 215-KthLargestElementinanArray/215-KthLargestElementinanArray.java
+++ 215-KthLargestElementinanArray/215-KthLargestElementinanArray.java
@@ -1,13 +1,26 @@
-// Last updated: 5/25/2026, 4:09:48 PM
-1import java.util.*;
-2class Solution {
-3    public int findKthLargest(int[] nums, int k) {
-4        PriorityQueue<Integer> q=new PriorityQueue<>();
-5        for(int num:nums){
-6            q.offer(num);
-7            if(q.size()>k)
-8                q.poll();
-9        }
-10        return q.peek();
-11    }
-12}
+// Last updated: 5/25/2026, 4:21:14 PM
+1class Solution {
+2    public String predictPartyVictory(String senate) {
+3        Queue<Integer> radiant=new LinkedList<>();
+4        Queue<Integer> dire =new LinkedList<>();
+5        int n=senate.length();
+6        for(int i=0;i<n;i++){
+7            if(senate.charAt(i)=='R'){
+8                radiant.offer(i);
+9            }else{
+10                dire.offer(i);
+11            }
+12        }
+13        while(!radiant.isEmpty()&&!dire.isEmpty()){
+14            int r=radiant.poll();
+15            int d=dire.poll();
+16            if(r<d){
+17                radiant.offer(r+n);
+18            }else{
+19                dire.offer(d+n);
+20            }
+21        }
+22        return radiant.isEmpty()?"Dire":"Radiant";
+23
+24    }
+25}
