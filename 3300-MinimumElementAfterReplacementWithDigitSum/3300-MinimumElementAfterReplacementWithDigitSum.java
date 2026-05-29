b3d57e68aaf27bf48eb2520eb38ca4f2b0983097
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-05-29 14:43:14 +0530

    File updated at 5/29/2026, 2:43:17 PM

--- 3300-MinimumElementAfterReplacementWithDigitSum/3300-MinimumElementAfterReplacementWithDigitSum.java
+++ 3300-MinimumElementAfterReplacementWithDigitSum/3300-MinimumElementAfterReplacementWithDigitSum.java
@@ -1,20 +1,36 @@
-// Last updated: 5/29/2026, 2:24:38 PM
+// Last updated: 5/29/2026, 2:43:17 PM
 1class Solution {
-2    public int minElement(int[] nums) {
-3        int min=Integer.MAX_VALUE;
-4        for(int i=0;i<nums.length;i++){
-5            int k=0;
-6            while(nums[i]!=0){
-7                int d=nums[i]%10;
-8                k+=d;
-9                nums[i]/=10;
-10            }
-11            nums[i]=k;
-12            if(min>nums[i]){
-13                min=nums[i];
-14            }
-15        }
-16        return min;
-17        
-18    }
-19}
+2    public boolean canFinish(int numCourses, int[][] prerequisites) {
+3        List<List<Integer>> adj=new ArrayList<>();
+4        for(int i=0;i<numCourses;i++){
+5            adj.add(new ArrayList<>());
+6        }
+7        int []indegree=new int[numCourses];
+8        for(int[] pre:prerequisites){
+9            int course=pre[0];
+10            int prereq=pre[1];
+11            adj.get(prereq).add(course);
+12            indegree[course]++;
+13        }
+14        Queue<Integer>q=new LinkedList<>();
+15        for(int i=0;i<numCourses;i++){
+16            if(indegree[i]==0){
+17                q.offer(i);
+18            }
+19        }
+20        int complete=0;
+21        while(!q.isEmpty()){
+22            int temp=q.poll();
+23            for(int neigh:adj.get(temp)){
+24                indegree[neigh]--;
+25                if(indegree[neigh]==0){
+26                    q.offer(neigh);
+27                }
+28            }
+29            complete++;
+30        }
+31        return complete==numCourses;
+32
+33
+34    }
+35}
