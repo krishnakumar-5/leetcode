845664997cf815d86aef71766c00de6ee85f7f8f
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-05-30 14:50:29 +0530

    File updated at 5/30/2026, 2:50:31 PM

--- 200-NumberofIslands/200-NumberofIslands.java
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -1,4 +1,4 @@
-// Last updated: 5/30/2026, 2:48:56 PM
+// Last updated: 5/30/2026, 2:50:31 PM
 1class Solution {
 2    public int findCenter(int[][] edges) {
 3        int a=(edges[0][0]);
@@ -6,11 +6,16 @@
 5        int res=0;
 6        for(int i=0;i<edges.length;i++){
 7            for(int j=0;j<edges[0].length;j++){
-8                if(a==(edges[i][j])||b==edges[i][j]){
-9                    res=edges[i][j];
-10                }
-11            }
-12        }
-13        return res;
-14    }
-15}
+8                if(a==edges[i][j]){
+9                    res=a;
+10                    break;
+11                }
+12                if(b==edges[i][j]){
+13                    res=b;
+14                    break;
+15                }
+16            }
+17        }
+18        return res;
+19    }
+20}
