0337182c3baacc842a611454b663d631df47aab0
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-02 21:17:25 +0530

    File updated at 6/2/2026, 9:17:25 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.java
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -1,21 +1,28 @@
-// Last updated: 6/2/2026, 8:48:23 PM
+// Last updated: 6/2/2026, 9:17:25 PM
 1class Solution {
-2    public boolean isHappy(int n) {
-3        HashSet<Integer> set = new HashSet<>();
+2    public int longestConsecutive(int[] nums) {
+3        Set<Integer> set = new HashSet<>();
 4
-5        while (n != 1 && !set.contains(n)) {
-6            set.add(n);
-7
-8            int sum = 0;
-9            while (n != 0) {
-10                int digit = n % 10;
-11                sum += digit * digit;
-12                n /= 10;
-13            }
-14
-15            n = sum;
-16        }
-17
-18        return n == 1;
-19    }
-20}
+5        for(int num : nums) {
+6            set.add(num);
+7        }
+8
+9        int longest = 0;
+10
+11        for(int num : set) {
+12            if(!set.contains(num - 1)) { 
+13                int curr = num;
+14                int len = 1;
+15
+16                while(set.contains(curr + 1)) {
+17                    curr++;
+18                    len++;
+19                }
+20
+21                longest = Math.max(longest, len);
+22            }
+23        }
+24
+25        return longest;
+26    }
+27}
