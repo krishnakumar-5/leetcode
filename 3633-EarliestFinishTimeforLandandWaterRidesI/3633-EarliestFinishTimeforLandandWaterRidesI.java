57b111fb93c850c1e9f22fccd175762ebf4e42cb
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-02 12:06:01 +0530

    File updated at 6/2/2026, 12:06:01 PM

--- 3633-EarliestFinishTimeforLandandWaterRidesI/3633-EarliestFinishTimeforLandandWaterRidesI.java
+++ 3633-EarliestFinishTimeforLandandWaterRidesI/3633-EarliestFinishTimeforLandandWaterRidesI.java
@@ -1,4 +1,4 @@
-// Last updated: 6/2/2026, 12:05:50 PM
+// Last updated: 6/2/2026, 12:06:01 PM
 1class Solution {
 2    public int earliestFinishTime(int[] landStartTime, int[] landDuration,int[] waterStartTime, int[] waterDuration) {
 3
