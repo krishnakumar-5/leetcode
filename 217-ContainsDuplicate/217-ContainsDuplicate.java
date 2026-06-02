b12ad922b4d8d88ca6a45de54a5c9194ac2b7ef9
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-02 20:48:23 +0530

    File updated at 6/2/2026, 8:48:23 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.java
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -1,12 +1,21 @@
-// Last updated: 4/5/2026, 7:18:35 PM
+// Last updated: 6/2/2026, 8:48:23 PM
 1class Solution {
-2    public boolean containsDuplicate(int[] nums) {
-3        Set<Integer> set=new HashSet<>();
-4        for(int num:nums){
-5            if(!set.add(num))
-6            return true;
-7        }
-8        return false;
-9       
-10    }
-11}
+2    public boolean isHappy(int n) {
+3        HashSet<Integer> set = new HashSet<>();
+4
+5        while (n != 1 && !set.contains(n)) {
+6            set.add(n);
+7
+8            int sum = 0;
+9            while (n != 0) {
+10                int digit = n % 10;
+11                sum += digit * digit;
+12                n /= 10;
+13            }
+14
+15            n = sum;
+16        }
+17
+18        return n == 1;
+19    }
+20}
