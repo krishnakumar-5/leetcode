7b6439e0b51e0f02dfab2c597d384f37371170ec
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-05 21:41:57 +0530

    File updated at 6/5/2026, 9:41:56 PM

--- 1019-NextGreaterNodeInLinkedList/1019-NextGreaterNodeInLinkedList.java
+++ 1019-NextGreaterNodeInLinkedList/1019-NextGreaterNodeInLinkedList.java
@@ -1,30 +1,35 @@
-// Last updated: 6/5/2026, 9:40:52 PM
+// Last updated: 6/5/2026, 9:41:56 PM
 1class Solution {
 2    public int[] nextLargerNodes(ListNode head) {
 3
-4        List<Integer> list = new ArrayList<>();
-5
-6        while (head != null) {
-7            list.add(head.val);
-8            head = head.next;
-9        }
-10
-11        int n = list.size();
-12        int[] ans = new int[n];
-13
-14        Stack<Integer> st = new Stack<>();
-15
-16        for (int i = 0; i < n; i++) {
-17
-18            while (!st.isEmpty() &&
-19                   list.get(i) > list.get(st.peek())) {
-20
-21                ans[st.pop()] = list.get(i);
-22            }
-23
-24            st.push(i);
-25        }
-26
-27        return ans;
-28    }
-29}
+4        int n = 0;
+5        ListNode temp = head;
+6
+7        while (temp != null) {
+8            n++;
+9            temp = temp.next;
+10        }
+11
+12        int[] values = new int[n];
+13        temp = head;
+14
+15        for (int i = 0; i < n; i++) {
+16            values[i] = temp.val;
+17            temp = temp.next;
+18        }
+19
+20        int[] ans = new int[n];
+21        Stack<Integer> st = new Stack<>();
+22
+23        for (int i = 0; i < n; i++) {
+24
+25            while (!st.isEmpty() && values[i] > values[st.peek()]) {
+26                ans[st.pop()] = values[i];
+27            }
+28
+29            st.push(i);
+30        }
+31
+32        return ans;
+33    }
+34}
