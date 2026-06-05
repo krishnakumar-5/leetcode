c5c6afbac4f77d510fe14e9ec57f8065f97b034b
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-05 09:34:14 +0530

    File updated at 6/5/2026, 9:34:13 AM

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.java
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.java
@@ -1,66 +1,39 @@
-// Last updated: 3/24/2026, 10:17:51 PM
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode reverseKGroup(ListNode head, int k) {
-        if (k <= 1 || head == null || head.next == null) return head;
-        //algorithm:
-        // 1. add dump head as group.prev
-        // 2. find kth node 
-        // 3. k+1th is group.next
-        // 4. reverse between group.prev and group.next (execlusive both)
-        // 5. return the 1st kth 
-
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode groupPrev = dummy;
-
-        while (true) {
-            ListNode kth = getKth(groupPrev, k);
-            if (kth == null) break;
-
-            groupPrev = reverseGroup(groupPrev, kth);
-        }
-
-        return dummy.next;
-    }
-
-   private ListNode getKth(ListNode curr, int k) {
-        while (curr != null && k > 0) {
-            curr = curr.next;
-            k--;
-        }
-        return curr;
-    }
-
-    // Reverses the group between groupPrev.next and kth (inclusive)
-    // Returns the new groupPrev for the next group,
-    // which is the old head of this group after reversal
-    private ListNode reverseGroup(ListNode groupPrev, ListNode kth) {
-        ListNode groupNext = kth.next;
-
-        ListNode prev = groupNext;
-        ListNode curr = groupPrev.next;
-
-        while (curr != groupNext) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-
-        ListNode newGroupTail = groupPrev.next; // old head
-        groupPrev.next = kth;                   // new head
-
-        return newGroupTail;
-    }
-        
-}
+// Last updated: 6/5/2026, 9:34:13 AM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12   public static ListNode mergeKLists(ListNode[] lists){
+13    return partion(lists,0,lists.length-1);
+14}
+15
+16public static ListNode partion(ListNode[] lists,int s,int e){
+17    if(s==e)  return lists[s];
+18    if(s<e){
+19        int q=(s+e)/2;
+20        ListNode l1=partion(lists,s,q);
+21        ListNode l2=partion(lists,q+1,e);
+22        return merge(l1,l2);
+23    }else
+24        return null;
+25}
+26
+27public static ListNode merge(ListNode l1,ListNode l2){
+28    if(l1==null) return l2;
+29    if(l2==null) return l1;
+30    if(l1.val<l2.val){
+31        l1.next=merge(l1.next,l2);
+32        return l1;
+33    }else{
+34        l2.next=merge(l1,l2.next);
+35        return l2;
+36    }
+37}
+38}
