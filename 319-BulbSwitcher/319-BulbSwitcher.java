f35c625ae03a288a4da57e8546a848025bbf4a36
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-06 15:45:13 +0530

    File updated at 6/6/2026, 3:45:13 PM

--- 319-BulbSwitcher/319-BulbSwitcher.java
+++ 319-BulbSwitcher/319-BulbSwitcher.java
@@ -1,14 +1,25 @@
-// Last updated: 6/6/2026, 2:37:19 PM
+// Last updated: 6/6/2026, 3:45:13 PM
 1class Solution {
-2    public int[][] transpose(int[][] matrix) {
+2    public void rotate(int[][] matrix) {
 3        int r=matrix.length;
 4        int c=matrix[0].length;
-5        int res[][]=new int[c][r];
-6        for(int i=0;i<c;i++){
-7            for(int j=0;j<r;j++){
-8                res[i][j]=matrix[j][i];
-9            }
-10        }
-11        return res;
-12    }
-13}
+5        for(int i=0;i<r;i++){
+6            for(int j=i+1;j<c;j++){
+7            	    int temp=matrix[i][j];
+8                matrix[i][j]=matrix[j][i];
+9                matrix[j][i]=temp;
+10            }
+11        }
+12        for(int i=0;i<r;i++){
+13            int l=0;
+14            int ri=r-1;
+15            while(l<ri){
+16                int temp=matrix[i][l];
+17                matrix[i][l]=matrix[i][ri];
+18                matrix[i][ri]=temp;
+19                l++;
+20                ri--;
+21            }
+22        }
+23    }
+24}
