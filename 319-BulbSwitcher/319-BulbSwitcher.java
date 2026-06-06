12d7b8620cc65502a2989e45e17881a979f98966
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-06 14:29:07 +0530

    File updated at 6/6/2026, 2:29:07 PM

--- 319-BulbSwitcher/319-BulbSwitcher.java
+++ 319-BulbSwitcher/319-BulbSwitcher.java
@@ -1,15 +1,13 @@
-// Last updated: 6/6/2026, 2:28:33 PM
+// Last updated: 6/6/2026, 2:29:07 PM
 1class Solution {
 2    public int maxSum(int[][] grid) {
-3        int r=grid.length;
-4        int c=grid[0].length;
-5        int max=Integer.MIN_VALUE;
-6        for(int i=0;i<r-2;i++){
-7            for(int j=0;j<c-2;j++){
-8                int sum=grid[i][j]+grid[i][j+1]+grid[i][j+2]+grid[i+1][j+1]+grid[i+2][j]+grid[i+2][j+1]+grid[i+2][j+2];
-9                max=Math.max(max,sum);
-10            }
-11        }
-12        return max;
-13    }
-14}
+3        int max=Integer.MIN_VALUE;
+4        for(int i=0;i<grid.length-2;i++){
+5            for(int j=0;j<grid[0].length-2;j++){
+6                int sum=grid[i][j]+grid[i][j+1]+grid[i][j+2]+grid[i+1][j+1]+grid[i+2][j]+grid[i+2][j+1]+grid[i+2][j+2];
+7                max=Math.max(max,sum);
+8            }
+9        }
+10        return max;
+11    }
+12}
