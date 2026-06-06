ce59e13793e050a703b0c5609d3bcee1aa5a3c87
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-06 14:37:19 +0530

    File updated at 6/6/2026, 2:37:19 PM

--- 319-BulbSwitcher/319-BulbSwitcher.java
+++ 319-BulbSwitcher/319-BulbSwitcher.java
@@ -1,15 +1,14 @@
-// Last updated: 6/6/2026, 2:29:40 PM
+// Last updated: 6/6/2026, 2:37:19 PM
 1class Solution {
-2    public int maxSum(int[][] grid) {
-3        int r=grid.length;
-4        int c=grid[0].length;
-5        int max=Integer.MIN_VALUE;
-6        for(int i=0;i<r-2;i++){
-7            for(int j=0;j<c-2;j++){
-8                int sum=grid[i][j]+grid[i][j+1]+grid[i][j+2]+grid[i+1][j+1]+grid[i+2][j]+grid[i+2][j+1]+grid[i+2][j+2];
-9                max=Math.max(max,sum);
-10            }
-11        }
-12        return max;
-13    }
-14}
+2    public int[][] transpose(int[][] matrix) {
+3        int r=matrix.length;
+4        int c=matrix[0].length;
+5        int res[][]=new int[c][r];
+6        for(int i=0;i<c;i++){
+7            for(int j=0;j<r;j++){
+8                res[i][j]=matrix[j][i];
+9            }
+10        }
+11        return res;
+12    }
+13}
