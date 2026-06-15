1bf4e9ac333319931827bc00e1815ff8b8c91fbc
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-15 16:15:55 +0530

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -0,0 +1,22 @@
+// Last updated: 6/15/2026, 4:15:55 PM
+1class Solution {
+2    public int trap(int[] height) {
+3        if(height==null || height.length<3){
+4            return 0;
+5        }
+6        int leftmax=height[0];
+7        Stack<Integer> s=new Stack<>();
+8        s.push(height[height.length-1]);
+9        for(int i=height.length-2;i>=2;i--){
+10            s.push(Math.max(height[i],s.peek()));
+11        }
+12        int water=0;
+13        for(int i=1;i<height.length-1;i++){
+14            int minHeight=Math.min(leftmax,s.peek());
+15            water=water+Math.max(0,minHeight-height[i]);
+16            s.pop();
+17            leftmax=Math.max(leftmax,height[i]);
+18        }
+19        return water;
+20    }
+21}
