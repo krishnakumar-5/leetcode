5b0898ad709620b9868350b2e6c1654db23856f8
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-16 09:53:56 +0530

    Create 3612-ProcessStringwithSpecialOperationsI

---
+++ 3612-ProcessStringwithSpecialOperationsI/3612-ProcessStringwithSpecialOperationsI.java
@@ -0,0 +1,23 @@
+// Last updated: 6/16/2026, 9:53:57 AM
+1class Solution {
+2    public String processStr(String s) {
+3        StringBuilder sb=new StringBuilder();
+4        for(int i=0;i<s.length();i++){
+5            char k=s.charAt(i);
+6            if(Character.isLetterOrDigit(k)){
+7                sb.append(k);
+8            }else if(k=='#'){
+9                if(sb.length()!=0){
+10                sb.append(sb);
+11                }
+12            }else if(k=='%'){
+13                sb.reverse();
+14            }else if(k=='*'){
+15                if(sb.length()!=0){
+16                sb.deleteCharAt(sb.length()-1);
+17                }
+18            }
+19        }
+20        return sb.toString();
+21    }
+22}
