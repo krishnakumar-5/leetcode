a60ffefbb9fe5712d5871c95d045f739c3945e39
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-17 14:28:11 +0530

    File updated at 6/17/2026, 2:28:11 PM

--- 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
+++ 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
@@ -1,8 +1,8 @@
-// Last updated: 6/17/2026, 2:26:44 PM
+// Last updated: 6/17/2026, 2:28:11 PM
 1class Solution {
 2    public int divide(int dividend, int divisor) {
 3        if(dividend==-2147483648&&divisor==-1){
-4            return 2147483647;
+4            return Integer.MAX_VALUE;
 5        }
 6        return dividend/divisor;
 7    }
