b4373f4d60d8b43c60af7a0e472bdd8d6d8b4999
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-17 22:25:18 +0530

    File updated at 6/17/2026, 10:25:18 PM

--- 50-Pow(x,n)/50-Pow(x,n).java
+++ 50-Pow(x,n)/50-Pow(x,n).java
@@ -1,6 +1,10 @@
-// Last updated: 6/17/2026, 10:13:35 PM
-1class Solution {
-2    public double myPow(double x, int n) {
-3        return Math.pow(x,n);
-4    }
-5}
+// Last updated: 6/17/2026, 10:25:18 PM
+1import java.math.*;
+2class Solution {
+3    public String multiply(String num1, String num2) {
+4        BigInteger k=new BigInteger(num1);
+5        BigInteger g=new BigInteger(num2);
+6        BigInteger h=k.multiply(g);
+7        return String.valueOf(h);
+8    }
+9}
