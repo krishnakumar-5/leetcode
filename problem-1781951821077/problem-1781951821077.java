c8957ad332c64afd90df3c5a8d8e96c147f86ab5
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-20 16:07:01 +0530

    Create

---
+++ problem-1781951821077/problem-1781951821077.java
@@ -0,0 +1,11 @@
+// Last updated: 6/20/2026, 4:07:01 PM
+1class Solution {
+2    public int countSegments(String s) {
+3        s=s.trim();
+4        if(s.length()==0){
+5            return 0;
+6        }
+7        String[] str=s.split("\\s+");
+8        return str.length;
+9    }
+10}
