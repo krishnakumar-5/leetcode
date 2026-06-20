f67df1b59409f5d5b7cf1a1eb6def1b2546ea1f7
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-20 16:03:43 +0530

    Create

---
+++ problem-1781951622780/problem-1781951622780.java
@@ -0,0 +1,16 @@
+// Last updated: 6/20/2026, 4:03:42 PM
+1class Solution {
+2    public int countSegments(String s) {
+3        if(s.length()==0){
+4            return 0;
+5        }
+6        String[] str=s.split("\\s+");
+7        int c=0;
+8        for(int i=0;i<str.length;i++){
+9            if(str[i].length()!=0){
+10                c++;
+11            }
+12        }
+13        return c;
+14    }
+15}
