98f3deaf4b7b63c7aa59e344d45feafc5e93703f
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-20 15:50:41 +0530

    Create

---
+++ problem-1781950840675/problem-1781950840675.java
@@ -0,0 +1,11 @@
+// Last updated: 6/20/2026, 3:50:40 PM
+1import java.math.*;
+2class Solution {
+3    public String addStrings(String num1, String num2) {
+4        BigInteger n=new BigInteger(num1);
+5        BigInteger m=new BigInteger(num2);
+6        BigInteger res=n.add(m);
+7        return String.valueOf(res);
+8
+9    }
+10}
