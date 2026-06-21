1dc9ce820196a9a2add486ed595975a076f44bd6
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-21 12:54:46 +0530

    File updated at 6/21/2026, 12:54:46 PM

--- 1833-MaximumIceCreamBars/1833-MaximumIceCreamBars.java
+++ 1833-MaximumIceCreamBars/1833-MaximumIceCreamBars.java
@@ -1,18 +1,13 @@
-// Last updated: 6/21/2026, 12:51:12 PM
+// Last updated: 6/21/2026, 12:54:46 PM
 1class Solution {
 2    public int maxIceCream(int[] costs, int coins) {
 3        Arrays.sort(costs);
-4        int sum=0,c=0;
-5        for(int i=0;i<costs.length;i++){
-6            if(sum+costs[i]<=coins){
-7                sum+=costs[i];
-8                c++;
-9            }else{
-10                if(c==1)
-11                    return 0;
-12                return c;
-13            }
-14        }
-15        return c;
-16    }
-17}
+4        int ans = 0;
+5        for (int cost : costs) {
+6            if (cost > coins) break;
+7            ans++;
+8            coins -= cost;
+9        }
+10        return ans;
+11    }
+12}
