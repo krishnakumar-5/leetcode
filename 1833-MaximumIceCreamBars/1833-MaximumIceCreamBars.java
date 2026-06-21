ef83e6c97d67e7f874c80e390a01034ab63dabcf
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-21 12:51:12 +0530

    File updated at 6/21/2026, 12:51:12 PM

--- 1833-MaximumIceCreamBars/1833-MaximumIceCreamBars.java
+++ 1833-MaximumIceCreamBars/1833-MaximumIceCreamBars.java
@@ -1,4 +1,4 @@
-// Last updated: 6/21/2026, 12:51:07 PM
+// Last updated: 6/21/2026, 12:51:12 PM
 1class Solution {
 2    public int maxIceCream(int[] costs, int coins) {
 3        Arrays.sort(costs);
