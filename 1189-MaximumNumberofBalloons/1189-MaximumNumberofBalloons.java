9fbfe02dd5b99014e252c3556eb86705986aa129
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-22 13:46:48 +0530

    File updated at 6/22/2026, 1:46:47 PM

--- 1189-MaximumNumberofBalloons/1189-MaximumNumberofBalloons.java
+++ 1189-MaximumNumberofBalloons/1189-MaximumNumberofBalloons.java
@@ -1,9 +1,9 @@
-// Last updated: 6/22/2026, 12:10:17 PM
+// Last updated: 6/22/2026, 1:46:47 PM
 1class Solution {
 2    public int maxNumberOfBalloons(String text) {
-3        if(text.charAt(0)=='y'&&text.length()>100){
-4            return 184;
-5        }
+3        // if(text.charAt(0)=='y'&&text.length()>100){
+4        //     return 184;
+5        // }
 6        int arr[]=new int[26];
 7        String B="balloon";
 8        int sum=0;
@@ -13,22 +13,27 @@
 12                arr[(int)(k-'a')]++;
 13            }
 14        }
-15        int g=1,c=0;
-16        while(g!=0){
-17            if(arr[0]==0||arr[1]==0||arr[11]-1==0||arr[13]==0||arr[14]-1==0){
-18                g=0;
-19            }else{
-20                arr[0]--;
-21                arr[1]--;
-22                arr[11]=arr[11]-2;;
-23                arr[13]--;
-24                arr[14]=arr[14]-2;
-25                c++;
-26            }
-27
-28        }
-29        return c;
-30        
-31        
-32    }
-33}
+15        arr[11]=arr[11]/2;
+16        arr[14]=arr[14]/2;
+17        int res=Math.min(Math.min(Math.min(Math.min(arr[0],arr[1]),arr[11]),arr[13]),arr[14]);
+18        return res;
+19        // int g=1,c=0;
+20        // while(g!=0){
+21        //     if(arr[0]==0||arr[1]==0||arr[11]-1==0||arr[13]==0||arr[14]-1==0){
+22        //         g=0;
+23        //     }else{
+24        //         arr[0]--;
+25        //         arr[1]--;
+26        //         arr[11]=arr[11]-2;;
+27        //         arr[13]--;
+28        //         arr[14]=arr[14]-2;
+29        //         c++;
+30        //     }
+31
+32        // }
+33        // return c;
+34        
+35        
+36        
+37    }
+38}
