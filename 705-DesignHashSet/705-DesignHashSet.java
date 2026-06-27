fe76dce99c2df3a1b07ecf70020d601a990e3f2a
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-27 14:02:51 +0530

    File updated at 6/27/2026, 2:02:51 PM

--- 705-DesignHashSet/705-DesignHashSet.java
+++ 705-DesignHashSet/705-DesignHashSet.java
@@ -1,6 +1,14 @@
-// Last updated: 6/27/2026, 1:55:59 PM
+// Last updated: 6/27/2026, 2:02:51 PM
 1class Solution {
 2    public String toLowerCase(String s) {
-3        return s.toLowerCase();
-4    }
-5}
+3        StringBuilder sb=new StringBuilder();
+4        for(int i=0;i<s.length();i++){
+5            if((int)s.charAt(i)<=90&&Character.isLetter(s.charAt(i))){
+6                sb.append((char)((int)s.charAt(i)+32));
+7            }else{
+8                sb.append(s.charAt(i));
+9            }
+10        }
+11        return sb.toString();
+12    }
+13}
