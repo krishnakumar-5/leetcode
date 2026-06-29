99123e5199404657f075018d6bb4bd2ac1803247
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-29 17:06:14 +0530

    File updated at 6/29/2026, 5:06:14 PM

--- 1967-NumberofStringsThatAppearasSubstringsinWord/1967-NumberofStringsThatAppearasSubstringsinWord.java
+++ 1967-NumberofStringsThatAppearasSubstringsinWord/1967-NumberofStringsThatAppearasSubstringsinWord.java
@@ -1,12 +1,14 @@
-// Last updated: 6/29/2026, 5:05:19 PM
+// Last updated: 6/29/2026, 5:06:14 PM
 1class Solution {
 2    public int numOfStrings(String[] patterns, String word) {
-3        int c=0;
-4        for(int i=0;i<patterns.length;i++){
-5            if(word.contains(patterns[i])){
-6                c++;
-7            }
-8        }
-9        return c;
-10    }
-11}
+3        int n=patterns.length;
+4        int c=0;
+5        for(int i=0;i<n;i++){
+6            String k=patterns[i];
+7            if(word.contains(k)){
+8                c++;
+9            }
+10        }
+11        return c;
+12    }
+13}
