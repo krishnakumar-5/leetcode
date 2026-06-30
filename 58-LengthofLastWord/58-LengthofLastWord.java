d1ceac1efab764a6714f9bbb7d203a32217d7bdf
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-06-30 18:10:29 +0530

    File updated at 6/30/2026, 6:10:30 PM

--- 58-LengthofLastWord/58-LengthofLastWord.java
+++ 58-LengthofLastWord/58-LengthofLastWord.java
@@ -1,4 +1,4 @@
-// Last updated: 6/30/2026, 6:10:23 PM
+// Last updated: 6/30/2026, 6:10:30 PM
 1class Solution {
 2    public int lengthOfLastWord(String s) {
 3        String str[]=s.trim().split(" ");
