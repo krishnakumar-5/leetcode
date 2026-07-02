7088a7bd1d771dabbc3eef49364611fe1c10bf7e
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-07-02 11:17:57 +0530

    File updated at 7/2/2026, 11:17:57 AM

--- 3559-NumberofWaystoAssignEdgeWeightsII/3559-NumberofWaystoAssignEdgeWeightsII.java
+++ 3559-NumberofWaystoAssignEdgeWeightsII/3559-NumberofWaystoAssignEdgeWeightsII.java
@@ -1,19 +1,17 @@
-// Last updated: 7/2/2026, 11:16:37 AM
-1class Solution {
-2    public int countPrimeSetBits(int left, int right) {
-3        int c=0;
-4        for(int i=left;i<=right;i++){
-5            String str=Integer.toBinaryString(i);
-6            int res=0;
-7            for(int j=0;j<str.length();j++){
-8                if(str.charAt(j)=='1'){
-9                    res++;
-10                }
-11            }
-12            if(res==3||res==2||res==5||res==7||res==11||res==13||res==17||res==19){
-13                c++;
-14            }
-15        }
-16        return c;
-17    }
-18}
+// Last updated: 7/2/2026, 11:17:57 AM
+class Solution 
+{
+public int countPrimeSetBits(int left, int right) 
+{
+int count = 0;
+for (int i = left; i <= right; i++) 
+{
+int bits = Integer.bitCount(i);
+if (bits == 2 || bits == 3 || bits == 5 || bits == 7 ||
+bits == 11 || bits == 13 || bits == 17 || bits == 19) {
+count++;
+}
+}
+return count;
+}
+}
