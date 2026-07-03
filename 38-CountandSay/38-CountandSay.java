3c0490d73f8b2a217b424ece43360d2ab2d87a05
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-07-03 17:56:15 +0530

    File updated at 7/3/2026, 5:56:15 PM

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,23 +1,15 @@
-// Last updated: 7/3/2026, 5:52:32 PM
+// Last updated: 7/3/2026, 5:56:15 PM
 1class Solution {
-2    public String[] uncommonFromSentences(String s1, String s2) {
-3        Set<String> set=new LinkedHashSet<>();
-4        String a[]=s1.split(" ");
-5        String []b=s2.split(" ");
-6        Map<String,Integer> map=new HashMap<>();
-7        for(int i=0;i<a.length;i++){
-8            map.put(a[i], map.getOrDefault(a[i], 0) + 1);
-9        }
-10        for(int i=0;i<b.length;i++){
-11            map.put(b[i], map.getOrDefault(b[i], 0) + 1);
-12        }
-13        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-14            if(entry.getValue()==1){
-15                set.add(entry.getKey());
-16            }
-17        }
-18        String[] array = set.toArray(new String[0]);
-19        return array; 
-20
-21    }
-22}
+2    public int finalValueAfterOperations(String[] operations) {
+3        int n=operations.length;
+4        int res=0;
+5        for(int i=0;i<n;i++){
+6            if(operations[i].charAt(0)=='+'||operations[i].charAt(operations[i].length()-1)=='+'){
+7                res++;
+8                continue;
+9            }
+10            res--;
+11        }
+12        return res;
+13    }
+14}
