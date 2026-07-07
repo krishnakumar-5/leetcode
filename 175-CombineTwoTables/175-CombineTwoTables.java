ce0b22cd3e085e6d7301bd76cc2da0918a2f1869
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-07-07 22:57:47 +0530

    File updated at 7/7/2026, 10:57:46 PM

--- 175-CombineTwoTables/175-CombineTwoTables.java
+++ 175-CombineTwoTables/175-CombineTwoTables.java
@@ -1,42 +1,24 @@
-// Last updated: 7/7/2026, 10:18:08 PM
+// Last updated: 7/7/2026, 10:57:46 PM
 1/**
-2 * Definition for singly-linked list.
-3 * public class ListNode {
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
 4 *     int val;
-5 *     ListNode next;
-6 *     ListNode() {}
-7 *     ListNode(int val) { this.val = val; }
-8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-9 * }
-10 */
-11class Solution {
-12    public void reorderList(ListNode head) {
-13        ListNode slow=head;
-14        ListNode fast=head;
-15        while(fast!=null&&fast.next!=null){
-16            slow=slow.next;
-17            fast=fast.next.next;
-18        }
-19        ListNode temp=slow.next;
-20        slow.next=null;
-21        ListNode prev=null;
-22        ListNode next=null;
-23        while(temp!=null){
-24            next=temp.next;
-25            temp.next=prev;
-26            prev=temp;
-27            temp=next;
-28        }
-29        ListNode h1=head;
-30        ListNode h2=prev;
-31        while(h2!=null){
-32            ListNode t1=h1.next;
-33            ListNode t2=h2.next;
-34            h1.next=h2;
-35            h2.next=t1;
-36            h1=t1;
-37            h2=t2;
-38        }
-39    }
-40
-41}
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public int maxDepth(TreeNode root) {
+18        if (root == null) {
+19            return 0;
+20        }
+21        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));        
+22    }
+23}
