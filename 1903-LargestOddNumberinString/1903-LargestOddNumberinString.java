ab81072093d9ee5373162c8adb7f6c68f005aa4d
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-07-10 18:16:00 +0530

    File updated at 7/10/2026, 6:16:01 PM

--- 1903-LargestOddNumberinString/1903-LargestOddNumberinString.java
+++ 1903-LargestOddNumberinString/1903-LargestOddNumberinString.java
@@ -1,11 +1,9 @@
-// Last updated: 7/10/2026, 6:14:37 PM
-1class Solution {
-2    public int maxProductDifference(int[] nums) {
-3        Arrays.sort(nums);
-4        if(nums.length<4){
-5            return 0;
-6        }
-7        int res=(nums[nums.length-1]*nums[nums.length-2])-(nums[0]*nums[1]);
-8        return res;
-9    }
-10}
+// Last updated: 7/10/2026, 6:16:01 PM
+class Solution {
+    public int maxProductDifference(int[] nums) {
+        Arrays.sort(nums);
+        int f=nums[0]*nums[1];
+        int l=nums[nums.length-1]*nums[nums.length-2];
+        return l-f;
+    }
+}
