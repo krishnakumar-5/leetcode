2c71c431795ace32881b69a9e0873c97205b5b37
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-07-10 18:14:37 +0530

    File updated at 7/10/2026, 6:14:37 PM

--- 1903-LargestOddNumberinString/1903-LargestOddNumberinString.java
+++ 1903-LargestOddNumberinString/1903-LargestOddNumberinString.java
@@ -1,15 +1,11 @@
-// Last updated: 7/10/2026, 6:10:22 PM
-class Solution {
-    public String largestOddNumber(String s) {
-
-        for (int i = s.length() - 1; i >= 0; i--) {
-            char ch = s.charAt(i);
-
-            if ((ch - '0') % 2 != 0) {
-                return s.substring(0, i + 1);
-            }
-        }
-
-        return "";
-    }
-}
+// Last updated: 7/10/2026, 6:14:37 PM
+1class Solution {
+2    public int maxProductDifference(int[] nums) {
+3        Arrays.sort(nums);
+4        if(nums.length<4){
+5            return 0;
+6        }
+7        int res=(nums[nums.length-1]*nums[nums.length-2])-(nums[0]*nums[1]);
+8        return res;
+9    }
+10}
