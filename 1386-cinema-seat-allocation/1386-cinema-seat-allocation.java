f08302787a34e6202a53e37d7f28e8b7e08366c1
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-08-19 18:08:11 +0530

    Time: 34 ms (25%), Space: 52.6 MB (52.1%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -5,25 +5,20 @@ class Solution {
 
         Map<Integer, Set<Integer>> rows = new HashMap<>();
 
-        // Store reserved seats row-wise
         for (int[] seat : reservedSeats) {
             rows.computeIfAbsent(seat[0], k -> new HashSet<>()).add(seat[1]);
         }
 
-        // Initially, every row can accommodate 2 families
         int ans = (n - rows.size()) * 2;
 
-        // Process only rows having reserved seats
         for (Map.Entry<Integer, Set<Integer>> entry : rows.entrySet()) {
 
             Set<Integer> li = entry.getValue();
 
             boolean left = false;
             boolean right = false;
-            boolean cf = false;
-            boolean cs = false;
+            boolean cen = false;
 
-            // Seats 2,3,4,5
             if (!li.contains(2) &&
                 !li.contains(3) &&
                 !li.contains(4) &&
@@ -32,21 +27,14 @@ class Solution {
                 left = true;
             }
 
-            // Seats 4,5
             if (!li.contains(4) &&
-                !li.contains(5)) {
-
-                cf = true;
-            }
-
-            // Seats 6,7
-            if (!li.contains(6) &&
+                !li.contains(5) &&
+                !li.contains(6) &&
                 !li.contains(7)) {
 
-                cs = true;
+                cen = true;
             }
 
-            // Seats 6,7,8,9
             if (!li.contains(6) &&
                 !li.contains(7) &&
                 !li.contains(8) &&
@@ -55,23 +43,19 @@ class Solution {
                 right = true;
             }
 
-            // Two families
             if (left && right) {
                 ans += 2;
             }
 
-            // Only left
             else if (left) {
                 ans += 1;
             }
 
-            // Only right
             else if (right) {
                 ans += 1;
             }
 
-            // Middle
-            else if (cf && cs) {
+            else if (cen) {
                 ans += 1;
             }
         }
