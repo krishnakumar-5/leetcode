5a8da76adf2d43264f1d6be315ea07194f3a3177
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-08-20 12:27:45 +0530

    Time: 1 ms (98.08%), Space: 46.6 MB (81.25%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -1,34 +1,23 @@
 class Solution {
     public int[] resultArray(int[] nums) {
-
-        int n = nums.length;
-
-        if (n <= 2)
-            return nums;
-
-        List<Integer> a = new ArrayList<>();
-        List<Integer> b = new ArrayList<>();
-
+        ArrayList<Integer> a=new ArrayList<>();
+        ArrayList<Integer> b=new ArrayList<>();
         a.add(nums[0]);
         b.add(nums[1]);
-
-        for (int i = 2; i < n; i++) {
-
-            if (a.get(a.size() - 1) > b.get(b.size() - 1))
-                a.add(nums[i]);
-            else
-                b.add(nums[i]);
+        for(int i=2;i<nums.length;i++){
+            if(a.get(a.size()-1)>b.get(b.size()-1)) a.add(nums[i]);
+            else b.add(nums[i]);
         }
+        int i=0;
+        for(int k=0;k<a.size();k++) {
+            nums[i]=a.get(k);
+            i++;
+        }
+        for(int k=0;k<b.size();k++){
+            nums[i]=b.get(k);
+            i++;
+        }
+        return nums;
 
-        int[] res = new int[n];
-        int k = 0;
-
-        for (int x : a)
-            res[k++] = x;
-
-        for (int x : b)
-            res[k++] = x;
-
-        return res;
     }
 }
