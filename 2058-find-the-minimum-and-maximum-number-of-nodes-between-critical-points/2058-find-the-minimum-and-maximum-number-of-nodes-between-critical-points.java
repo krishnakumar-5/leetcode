e42521a6e2770d95c329fa9b84d6b9c623d4d14d
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-08-31 14:51:10 +0530

    Time: 9 ms (23.01%), Space: 107.2 MB (26.07%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -14,26 +14,23 @@ class Solution {
         ArrayList<Integer> li = new ArrayList<>();
         ListNode temp = head.next;
         ListNode prev = head;
-        ListNode next = temp.next;
-        int lmax = 0, lmin = 0, c = 1;
+        int c = 1;
         while (temp.next != null) {
-            int pd = prev.val;
-            int td = temp.val;
-            int nd = next.val;
             c++;
-            if ((pd > td && nd > td) || (pd < td && nd < td)) {
+            if ((prev.val > temp.val && temp.next.val > temp.val) || (prev.val < temp.val && temp.next.val < temp.val)) {
                 li.add(c);
             }
             prev = temp;
-            temp = next;
-            next = temp.next;
+            temp = temp.next;
         }
         if (li.size() < 2)
             return res;
         res[1] = (li.get(li.size() - 1) - li.get(0));
         int min = Integer.MAX_VALUE;
         for (int i = 1; i < li.size() ; i++) {
-            min = Math.min(min,(li.get(i) - li.get(i - 1)));
+            if(min>(li.get(i)-li.get(i-1))){
+                min=(li.get(i) - li.get(i - 1));
+            }
         }
         res[0] = min;
         return res;
