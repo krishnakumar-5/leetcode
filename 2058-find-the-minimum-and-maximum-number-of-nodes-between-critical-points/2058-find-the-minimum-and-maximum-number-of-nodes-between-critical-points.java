206e6af9e3b32b394cd1c2a44d9f6a0957ec304d
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-08-31 14:47:23 +0530

    Time: 9 ms (23.01%), Space: 107.9 MB (16.32%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -10,31 +10,32 @@
  */
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        int res[]={-1,-1};
-        ArrayList<Integer> li=new ArrayList<>();
-        ListNode temp=head.next;
-        ListNode prev=head;
-        ListNode next=temp.next;
-        int lmax=0,lmin=0,c=1;
-        while(temp.next!=null){
-            int pd=prev.val;
-            int td=temp.val;
-            int nd=next.val;
+        int res[] = { -1, -1 };
+        ArrayList<Integer> li = new ArrayList<>();
+        ListNode temp = head.next;
+        ListNode prev = head;
+        ListNode next = temp.next;
+        int lmax = 0, lmin = 0, c = 1;
+        while (temp.next != null) {
+            int pd = prev.val;
+            int td = temp.val;
+            int nd = next.val;
             c++;
-            if((pd>td&&nd>td)||(pd<td&&nd<td)){
+            if ((pd > td && nd > td) || (pd < td && nd < td)) {
                 li.add(c);
             }
-            prev=temp;
-            temp=next;
-            next=temp.next;
+            prev = temp;
+            temp = next;
+            next = temp.next;
         }
-        if(li.size()<2) return res;
-        res[1]=(li.get(li.size()-1)-li.get(0));
-        int min=Integer.MAX_VALUE;
-        for(int i=0;i<li.size()-1;i++){
-            min=Math.min(min,Math.abs((li.get(i)-li.get(i+1))));
+        if (li.size() < 2)
+            return res;
+        res[1] = (li.get(li.size() - 1) - li.get(0));
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i < li.size() ; i++) {
+            min = Math.min(min,(li.get(i) - li.get(i - 1)));
         }
-        res[0]=min;
+        res[0] = min;
         return res;
 
     }
